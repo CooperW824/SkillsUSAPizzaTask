d557e470556270900cf29fe8caadbcc2e082a295
Cooper Werner <cooperwerner824@gmail.com>
2021-04-16 19:27:18 -0400

    Design and Input Validation

--- SkillsUSAPizzaTask/Form1.cs
+++ SkillsUSAPizzaTask/Form1.cs
@@ -16,10 +16,52 @@ namespace SkillsUSAPizzaTask
         {
             InitializeComponent();
         }
+        //declaring global vars here
 
-        private void Form1_Load(object sender, EventArgs e)
+        int pizza; //pizza counter variable used for hiding aspects of the form and for displaying number of pizzas
+        string customer; //customer name var
+        private void exitButton_Click(object sender, EventArgs e)
         {
+            this.Close();
+        }
 
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CALC!");
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("CLEAR!");
+        }
+
+        private void numPizzas_TextChanged(object sender, EventArgs e)
+        {
+            
+
+            
+        }
+
+        private void orderContinue_Click(object sender, EventArgs e)
+        {
+           
+            bool inputValid = false;
+            
+                inputValid = Int32.TryParse(numPizzas.Text, out pizza); // checking if the input is a 
+            //valid Int32 returns input valid becomes false if not valid, and become true if is, 
+            //pizza also gets the int value if valid
+                
+
+                if (!inputValid || pizza > 2)
+                {
+                    MessageBox.Show("Please Enter a Valid Number of Pizzas.");
+                }
+          
+        }
+
+        private void customerName_TextChanged(object sender, EventArgs e)
+        {
+            customer = customerName.Text;
         }
     }
 }
