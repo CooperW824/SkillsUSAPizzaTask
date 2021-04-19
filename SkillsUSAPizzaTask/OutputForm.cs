5e80d3119bc160f9f18772feb7bd56f8ccd2dd2e
Cooper Werner <cooperwerner824@gmail.com>
2021-04-19 17:21:23 -0400

    Version 1.0 Release Build
    + Finished Input Validation
    + Provided completed output
    + Finished OutputForm.cs
    +Fixed Bugs
    + Built Debug and Release EXEs

--- SkillsUSAPizzaTask/OutputForm.cs
+++ SkillsUSAPizzaTask/OutputForm.cs
@@ -18,14 +18,31 @@ namespace SkillsUSAPizzaTask
 
         private void orderButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Ordered!");
+            MessageBox.Show("Pizza Ordered!");
             this.Close();
         }
 
-        private void clearButton_Click(object sender, EventArgs e)
+
+        private void OutputForm_Load(object sender, EventArgs e)
         {
-            MessageBox.Show("Cleared!");
-            this.Close();
+            int pizzaAmt = MainForm.pizza;
+            float totalPrice = MainForm.totalPrice;
+
+            if(pizzaAmt == 2)
+            {
+                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output;
+                pizza2Output.Text = "Pizza 2: \n" + MainForm.pizza2output;
+                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza; 
+                totalOutput.Text = "Total: $" + totalPrice.ToString();
+                pizza2Output.Visible = true;
+            }
+            else
+            {
+                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output;
+                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza;
+                totalOutput.Text = "Total: $" + totalPrice.ToString();
+                pizza2Output.Visible = false;
+            }
         }
     }
 }
