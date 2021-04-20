ec82180f65838e5161ec43e71acff462cdd09ddf
Cooper Werner <cooperwerner824@gmail.com>
2021-04-20 10:56:30 -0400

    Finished Documentation for the Project, and rebuilt
    +Finished documentation on Form1.cs and OutputForm.cs
    +Rebuild Debug and Release version, (no functional changes)

--- SkillsUSAPizzaTask/OutputForm.cs
+++ SkillsUSAPizzaTask/OutputForm.cs
@@ -10,38 +10,40 @@ namespace SkillsUSAPizzaTask
             InitializeComponent();
         }
 
+        //on closeButton click event close the current form
         private void closeButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //close the current form 
 
         }
 
+        //on orderButton click, "Order" the pizza, display that to the user, and close the current form
         private void orderButton_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Pizza Ordered!");
-            this.Close();
+            MessageBox.Show("Pizza Ordered!"); //tell the user the pizza(s) have been "ordered"
+            this.Close(); //close the current form
         }
 
 
         private void OutputForm_Load(object sender, EventArgs e)
         {
-            int pizzaAmt = MainForm.pizza;
-            float totalPrice = MainForm.totalPrice;
+            int pizzaAmt = MainForm.pizza; //get the number of pizzas from the mainform
+            float totalPrice = MainForm.totalPrice; //get the total price from the main form
 
-            if(pizzaAmt == 2)
+            if(pizzaAmt == 2) //if there are two pizzas output both of their infomation
             {
-                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output;
-                pizza2Output.Text = "Pizza 2: \n" + MainForm.pizza2output;
-                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza; 
-                totalOutput.Text = "Total: $" + totalPrice.ToString();
-                pizza2Output.Visible = true;
+                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output; //output pizza 1 info
+                pizza2Output.Text = "Pizza 2: \n" + MainForm.pizza2output;//output pizza 2 info
+                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza; //output customer name and num pizzas ordered
+                totalOutput.Text = "Total: $" + totalPrice.ToString(); //output total price
+                pizza2Output.Visible = true; //make the pizza 2 output visible
             }
-            else
+            else //if there is only 1 pizza ordered
             {
-                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output;
-                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza;
-                totalOutput.Text = "Total: $" + totalPrice.ToString();
-                pizza2Output.Visible = false;
+                pizza1Output.Text = "Pizza 1:\n" + MainForm.pizza1output; // output pizza 1 information
+                outputLabel.Text = "Customer Name: " + MainForm.customer + "\n Pizzas Ordered: " + MainForm.pizza; //output customer name and pizzas ordered
+                totalOutput.Text = "Total: $" + totalPrice.ToString(); //output total price
+                pizza2Output.Visible = false; //hide the second pizza output
             }
         }
     }
